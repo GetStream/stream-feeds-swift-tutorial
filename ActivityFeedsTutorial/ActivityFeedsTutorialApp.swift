bcb61163fd01ae3e535dc3105a6cadcde60892cb
Toomas Vahter <toomas.vahter@getstream.io>
2025-12-10 11:38:35 +0200

    Connect to the Stream API

--- ActivityFeedsTutorial/ActivityFeedsTutorialApp.swift
+++ ActivityFeedsTutorial/ActivityFeedsTutorialApp.swift
@@ -2,6 +2,7 @@
 // Copyright © 2025 Stream.io Inc. All rights reserved.
 //
 
+import StreamFeeds
 import SwiftUI
 
 @main
@@ -9,6 +10,26 @@ struct ActivityFeedsTutorialApp: App {
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environment(\.feedsClient, FeedsClient.current)
         }
     }
+}
+
+extension FeedsClient {
+    @MainActor static var current: FeedsClient = {
+        LogConfig.level = .info
+        let credentials = UserCredentials.current
+        return FeedsClient(
+            apiKey: APIKey(credentials.apiKey),
+            user: User(
+                id: credentials.id,
+                name: credentials.name
+            ),
+            token: UserToken(rawValue: credentials.token)
+        )
+    }()
+}
+
+extension EnvironmentValues {
+    @Entry var feedsClient: FeedsClient = FeedsClient.current
 }
--- ActivityFeedsTutorial/RootView.swift
+++ ActivityFeedsTutorial/RootView.swift
@@ -2,16 +2,35 @@
 // Copyright © 2025 Stream.io Inc. All rights reserved.
 //
 
+import StreamFeeds
 import SwiftUI
 
 struct RootView: View {
+    @Environment(\.feedsClient) var client
+    @State private var isConnected = false
+    
     var body: some View {
-        TabView {
-            Tab("Home", systemImage: "house") {
-                HomeView()
+        VStack {
+            if isConnected {
+                TabView {
+                    Tab("Home", systemImage: "house") {
+                        HomeView()
+                    }
+                    Tab("Explore", systemImage: "magnifyingglass") {
+                        ExploreView()
+                    }
+                }
+            } else {
+                ProgressView("Connecting to the Stream API")
             }
-            Tab("Explore", systemImage: "magnifyingglass") {
-                ExploreView()
+        }
+        .task(id: client.user.id) {
+            do {
+                try await client.connect()
+                log.info("✅ User \(client.user.id) connected successfully")
+                isConnected = true
+            } catch {
+                log.error("Failed to connect", error: error)
             }
         }
     }
